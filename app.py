cb416cd605987915b341bad12ff3d4a9bca68233
Umair-Siddique <umairsiddique180@gmail.com>
2026-02-25 03:34:47 +0500

    updated cors

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def create_app():
 
     CORS(app, 
          supports_credentials=True, 
-         origins=["https://governance-saas.vercel.app"],
+         origins=["https://governance-saas.vercel.app", "http://localhost:5173"],
          methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
          allow_headers=['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
          expose_headers=['Content-Type', 'Authorization']
